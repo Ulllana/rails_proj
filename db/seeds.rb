6b3482073ff1ac0e7399f0aeb9800226d7771b11
Ulllana <ulyanalana2003@gmail.com>
2024-05-27 00:14:54 +0300

    Nice cat bro

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,18 @@
 unless User.any?
     user = User.create(name: 'Bob', email: 'bob@example.com')
 
-    catlist = user.catlists.create(name: 'My Catlist')
+    catlist = user.catlists.create(name: 'Bob Catlist')
 
-    catlist.cats.create([{name: 'Mr Frash', description: 'Nice cat'}, {name: 'Sigma', description: 'Sigma'}])
+    catlist.cats.create([{name: 'Mr Fresh', description: 'Nice cat'}, {name: 'Sigma', description: 'Sigma'}])
+
+    user.save
+
+    user = User.create(name: 'Jimmy', email: 'jimmy@example.com')
+
+    catlist = user.catlists.create(name: 'Jimmy Catlist')
+
+    catlist.cats.create([{name: 'Mr Unfresh', description: 'Smell cat'}, {name: 'Mussy', description: 'Pussy'},
+    {name: 'Aboba', description: 'Amogus'}])
 
     user.save
 end

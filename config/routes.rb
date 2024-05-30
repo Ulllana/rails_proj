61f1c727ce132a7c104df01c438fced1d93115ca
Кирий Семён <kirii@sfedu.ru>
2024-05-31 01:43:20 +0300

    Can add new cats

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
   get 'home/index'
   resources :catlists
   root 'home#index'
+
   get 'new_cat', to: 'home#new_cat', as: 'new_cat'
   get 'show_all_records', to: 'home#show_all_records', as: 'show_all_records'
+
+  get 'show_all_records', to: 'users#show_all_records', as: 'show_all_records'
+
 end

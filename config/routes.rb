804e6f5d1c7f2cfb149733106942d7a89c5f1c7d
Кирий Семён <kirii@sfedu.ru>
2024-05-31 01:39:02 +0300

    Can add new cats

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,9 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-
   get 'signup', to: 'registrations#new'
   post 'signup', to: 'registrations#create'
   get 'home/index'
-
   root 'home#index'
   get 'new_cat', to: 'home#new_cat', as: 'new_cat'
   post 'create_cat', to: 'home#create_cat', as: 'create_cat'

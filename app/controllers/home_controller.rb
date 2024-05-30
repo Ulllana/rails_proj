5732255119dd39dcf33c7c429a00f4e403ebb406
Ulllana <ulyanalana2003@gmail.com>
2024-05-30 17:03:59 +0300

    new functions

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,16 @@
 class HomeController < ApplicationController
+    def show
+        @catlists = Catlist.find(params[:id])
+    end
+
+    def new_cat
+    end
+    
+    def create
+        @catlist = Catlist.new(params.require(:cat).permit(:name, :description))
+        @catlist.save
+        redirect_to @catlist
+    end
 
     def index
         @users = User.all
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  resources :catlists
   root 'home#index'
+  get 'new_cat', to: 'home#new_cat', as: 'new_cat'
   get 'show_all_records', to: 'home#show_all_records', as: 'show_all_records'
 end

e9104b014aef73c0748f7f5c62a44778ba328812
Кирий Семён <116794037+0monRa@users.noreply.github.com>
2024-05-31 01:50:13 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,11 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-
   get 'signup', to: 'registrations#new'
   post 'signup', to: 'registrations#create'
   get 'home/index'
-  resources :catlists
   root 'home#index'
-
   get 'new_cat', to: 'home#new_cat', as: 'new_cat'
+  post 'create_cat', to: 'home#create_cat', as: 'create_cat'
   get 'show_all_records', to: 'home#show_all_records', as: 'show_all_records'
-
-  get 'show_all_records', to: 'users#show_all_records', as: 'show_all_records'
-
 end
